bee34361878f8b087e87160f1639e1aa758379e6
damianattsu <damianattsu@gmail.com>
2022-03-21 23:03:33 +0900

    fixed code

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 export default function Home() {
 	return (
 		<Layout title="Home" content="Homeページです">
-			<h1 className="text-4xl">Welcom to Next.js</h1>
+			<h1 className="text-4xl">Hi, Next.js</h1>
 		</Layout>
 	);
 }
