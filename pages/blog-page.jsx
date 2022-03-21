3f5f1b520d4c25561358f0c27b505bb4e4bf5e69
damianattsu <damianattsu@gmail.com>
2022-03-21 23:00:10 +0900

    changed to function

--- pages/blog-page.jsx
+++ pages/blog-page.jsx
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import Post from "../components/Post";
 import { getAllPostData } from "../lib/posts";
 
-const Blog = ({ posts }) => {
+export default function Blog({ posts }) {
 	return (
 		<Layout title="Blog" content="Blogページです">
 			<ul className="m-10">
@@ -13,8 +13,7 @@ const Blog = ({ posts }) => {
 			</ul>
 		</Layout>
 	);
-};
-export default Blog;
+}
 
 export async function getStaticProps() {
 	const posts = await getAllPostData();
